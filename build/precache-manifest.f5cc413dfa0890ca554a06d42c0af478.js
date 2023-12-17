9494cab0a82f25b8e2d0ed248f23998f9ebb05e2
Jasurbek <goldenwashington159@gmail.com>
2023-12-17 22:45:21 +0600

    Fixing the bugs

---
+++ build/precache-manifest.f5cc413dfa0890ca554a06d42c0af478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee830ed545577930e1d6a37abcd663b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfee0ce931bc891a4cdc",
+    "url": "/static/css/main.67db74a9.chunk.css"
+  },
+  {
+    "revision": "7ccb9089acef15d5ec5f",
+    "url": "/static/js/2.58a8787f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.58a8787f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfee0ce931bc891a4cdc",
+    "url": "/static/js/main.e346c86d.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
